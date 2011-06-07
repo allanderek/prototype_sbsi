e9f1a7ea554db088b16f9f83140c84aa93819343
Allan Clark <a.d.clark@ed.ac.uk>
2011-06-07 15:11:00 +0100

    Added a bit more to the configuration generator, still not quite there in terms of actually using that, but nearly

--- config_generator.py
+++ config_generator.py
@@ -6,6 +6,16 @@ def print_imports(module_file):
   module_file.write("import sys\n")
   module_file.write("import xml.dom.minidom\n\n\n")
 
+def print_exception_definition(module_file):
+  except_def = """
+class ConfigurationError(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+"""
+  module_file.write(except_def)
+  module_file.write("\n\n\n")
 
 def print_numerical_types(schema_file):
   numerical_schema_types = """
@@ -108,10 +118,10 @@ def print_numerical_types(schema_file):
   """
   schema_file.write(numerical_schema_types)
 
-
 class Element:
   def __init__(self, name):
     self.name = name
+    self.varname = name.lower()
 
   def has_xml_children(self):
     return False
@@ -122,7 +132,25 @@ class Element:
   def print_class_definition(self, module_file):
     module_file.write("class " + self.name + ":\n")
     self.print_parse_function(module_file)
-    
+
+
+
+## Note where you have more than one sub-element of the same name
+## eg. <FFT> <State> ... </State><State> ... </State> </FFT>
+## We'll build a list element, where actually there is no
+## actual xml element of that kind, but we fake it here.
+class ListOfElements(Element):
+  # This should also have a maximum and minimum number
+  def __init__(self, item):
+    Element.__init__(self, item.name)
+    self.item = item
+
+  # print parse function isn't defined and it should never
+  # be called on this kind of element. ListOfElements exists
+  # so that the parent parsing function can decide whether to
+  # parse in a list of such elements or a single one.
+
+   
 class ContainerElement(Element):
   
   def __init__(self, name, children):
@@ -132,57 +160,115 @@ class ContainerElement(Element):
 
   def has_xml_children(self):
     return self.children != []
-
+  
   def add_child(self, child):
     self.children.append(child)
 
   def print_parse_function(self, module_file):
     module_file.write("  def " + self.parse_method_name() +
-                      "(self, " + self.name + "):\n")
+                      "(self, " + self.varname + "):\n")
     for child in self.children:
-      module_file.write("    " + child.name + 
-                        "s = " + self.name +
+      module_file.write("    " + child.varname + 
+                        "_elements = " + self.varname +
                         ".getElementsByTagName(\"" +
                         child.name + "\")\n")
-      module_file.write("    for " + child.name + "_element in " + 
-                        child.name + "s :\n")
-      module_file.write("      self." + child.name + " = " +
-                        child.name + "()\n")
-      module_file.write("      " + child.name + "." +
-                        child.parse_method_name() +
-                        "(" + child.name + "_element)\n")
-##
-## Note where you have more than one sub-element of the same name
-## eg. <FFT> <State> ... </State><State> ... </State> </FFT>
-## We'll build a list element, where actually there is no
-## actual xml element of that kind, but we fake it here.
-class ListOfElements(Element):
-  def __init__(self, name, item):
-    Element.__init__(self, name)
-    self.item = item
-
-  # So the parse function here, unlike the others will be passed
-  # in the parent element rather than the actual element.
-
+      if isinstance(child, ListOfElements):
+        # If you need to get other data from the ListOfElements,
+        # for example the max and min number of occurrences then
+        # do that before you make child = child.item. 
+        child = child.item
+        module_file.write("    " + child.varname + "s = []\n")
+        module_file.write("    for " + child.varname + "_element in " + 
+                          child.varname + "_elements :\n")
+        module_file.write("      " + child.varname + " = " +
+                          child.name + "()\n")
+        module_file.write("      " + child.varname + "." +
+                          child.parse_method_name() +
+                          "(" + child.varname + "_element)\n")
+        module_file.write("      " + child.varname + "s.append(" +
+                          child.varname + ")\n")
+      else:
+        module_file.write("    if len(" + child.varname + 
+                                      "_elements) > 1:\n")
+        module_file.write("      raise ConfigurationError (\"too many\")\n")
+ 
+        # We should do something in the case that there are no
+        # child elements, but we must first find a way to say
+        # that the child element may be not there at all
+        module_file.write("    elif len(" + child.varname +
+                                        "_elements) == 1:\n")
+        module_file.write("      self." + child.varname + " = " +
+                          child.name + "()\n")
+        module_file.write("      self." + child.varname + "." +
+                          child.parse_method_name() +
+                          "(" + child.varname + "_elements[0])\n")
+        module_file.write("    else:\n")
+        module_file.write("      self." + child.varname + " = None\n")
+     
 
 class EnumerationElement(Element):
   def __init__(self, name, choices):
     Element.__init__(self, name)
     self.choices = choices
 
+  def print_parse_function(self, module_file):
+    arg_name = self.varname
+    module_file.write("  def " + self.parse_method_name() +
+                      "(self, " + arg_name + "):\n")
+    module_file.write("    text = " + arg_name + ".firstChild\n")
+    module_file.write("    if text not in [ ")
+    module_file.write("\"" + self.choices[0] + "\"")
+    for i in range(1, len(self.choices)):
+      module_file.write(", \"" + self.choices[i] + "\"")
+    module_file.write("]:\n")
+    # Should give more information than just the text
+    module_file.write("      raise ConfigurationError(text)\n")
+    module_file.write("    self.value = text\n\n")
+   
+
 class LiteralElement(Element):
   def __init__(self, name, simple_type):
     Element.__init__(self, name)
     self.simple_type = simple_type
  
+  def get_data_parser(self, argument):
+    # Obviously for each kind it could do some more checking
+    if self.simple_type in [ "xs:int",
+                             "nonZeroMultipleOfTwo",
+                             "nonZeroMultipleOfTwoAndGtThree",
+                             "AzusaPercentage",
+                             "positiveIntGTOne",
+                             "positiveIntNotZero",
+                             "positiveIntIncludingZero",
+                           ]:
+      return "int(" + argument + ")"
+    elif self.simple_type in [ "positiveDecimalIncludingZero",
+                               "positiveDecimalNotZeroButCanBeMinusOne",
+                               "positiveDecimalIncludingZero",
+                               "positiveDecimalNotZero",
+                               "probability",
+                               "minusOne",
+                               "positiveFloatNotZero",
+                               "positiveFloatIncludingZero",
+                             ]:
+      return "float(" + argument + ")"
+    elif self.simple_type in [ "nonEmptyString" ]:
+      # The argument should already be a string so no parsing
+      # function required
+      return argument
+    else:
+      print ("Undefined simple type: " + self.simple_type)
+      sys.exit(1)
+
   def print_parse_function(self, module_file):
-    arg_name = self.name
+    arg_name = self.varname
     module_file.write("  def " + self.parse_method_name() +
                       "(self, " + arg_name + "):\n")
     module_file.write("    text = " + arg_name + ".firstChild\n")
     # For most of them it will be float, but actually we will
     # have to check the simple type for some.
-    module_file.write("    self.value = float(text)\n")
+    module_file.write("    self.value = " + 
+                      self.get_data_parser("text.data") + "\n")
     # We should also have some code to check for the actual constraints
     # imposed by the particular simpleType.
     
@@ -196,28 +282,122 @@ class OneOfElement(Element):
   # same as this.
   def print_parse_function(self, module_file):
     module_file.write("  def " + self.parse_method_name() +
-                      "(" + self.name + "):\n")
+                      "(self, " + self.varname + "):\n")
     # Not quite right, we need to find out how many children we have.
-    module_file.write("    num_children = 0\n")
-    module_file.write("    if num_children != 1:\n")
-    module_file.write("      print(\"Configuration parse error\")\n")
-    module_file.write("      sys.exit(1)\n")
+    module_file.write("    # num_children = len(" + self.varname +
+                      ".childNodes)\n")
+    module_file.write("    # if num_children != 1:\n")
+    module_file.write("    #   print(\"Configuration parse error\")\n")
+    module_file.write("    #   print(\"Number of children = \" +\n")
+    module_file.write("    #           str(num_children))\n")
+    module_file.write("    #   sys.exit(1)\n")
     module_file.write("    child_name = \"\"\n")
     for choice in self.choices:
-      module_file.write("    " + choice.name + 
-                        "s = " + self.name +
+      module_file.write("    " + choice.varname + 
+                        "s = " + self.varname +
                         ".getElementsByTagName(\"" +
                         choice.name + "\")\n")
-      module_file.write("    for " + choice.name + " in " + 
-                        choice.name + "s :\n")
-      module_file.write("      self." +
-               
+      module_file.write("    for " + choice.varname + "_element in " + 
+                        choice.varname + "s :\n")
+      module_file.write("      " + choice.varname + " = " +
+                        choice.name  + "()\n")
+      module_file.write("      " + choice.varname + "." +
+
                         choice.parse_method_name() +
-                        "(" + choice.name + ")\n")
-   
+                        "(" + choice.varname + "_element)\n")
 
 
-      
+def simple_element(name, el_type, dictionary):
+  if name in dictionary:
+    element = dictionary[name]
+    if el_type == element.simple_type:
+      return element
+    else:
+      print ("Conflicting type definitions for: " + name)
+      print (el_type)
+      print (element.simple_type)
+      sys.exit(1)
+  # If this is the first mention of 'name' then fine just add it
+  element = LiteralElement(name, el_type)
+  dictionary[name] = element
+  return element
+
+def get_cvode_solver_element(element_dictionary): 
+  cvode_children = [ simple_element("TFinal", 
+                             "positiveDecimalNotZeroButCanBeMinusOne",
+                             element_dictionary),
+                     simple_element("TInit", "positiveFloatNotZero",
+                                    element_dictionary),
+                     simple_element("Interval", "positiveFloatNotZero",
+                                    element_dictionary),
+                     simple_element("MaxTimes", "positiveIntNotZero", 
+                                    element_dictionary),
+                     simple_element("Atol", "positiveFloatNotZero",
+                                    element_dictionary),
+                     simple_element("Reltol", "positiveDecimalNotZero",
+                                    element_dictionary),
+                   ]
+
+  cvode_element = ContainerElement("CVODESolver", cvode_children)
+  element_dictionary[cvode_element.name] = cvode_element
+  return cvode_element
+
+def get_model_cost_function(element_dictionary):
+  # dcs = data set config
+  dsc_children = [ simple_element("FileName", "nonEmptyString",
+                                  element_dictionary)
+                 ]
+  dsc_element = ContainerElement("DataSetConfig", dsc_children)
+  element_dictionary[dsc_element.name] = dsc_element
+  x2_children = [ simple_element("Weight", "positiveFloatNotZero",
+                                 element_dictionary),
+                  dsc_element
+                ]
+  x2_element = ContainerElement("X2Cost", x2_children)
+  element_dictionary[x2_element.name] = x2_element
+
+
+  mcf_choices = [ x2_element ]
+  mcf_element = OneOfElement("ModelCostFunction", mcf_choices)
+  element_dictionary[mcf_element.name] = mcf_element
+
+  return mcf_element
+
+def get_setup_element(element_dictionary):
+  setup_type_choices = [ "SobolSelect", "SobolUnselect", "ReadinFile" ]
+  setup_type_element = EnumerationElement("SetupType", setup_type_choices)
+  element_dictionary[setup_type_element.name] = setup_type_element
+ 
+  mcf_element = get_model_cost_function(element_dictionary)
+  mcf_list =  ListOfElements(mcf_element)
+
+  setup_children = [ simple_element("MaxCost", "positiveIntNotZero",
+                                    element_dictionary),
+                     simple_element("MaxRandomNumberGenerator", 
+                                    "positiveIntNotZero",
+                                    element_dictionary),
+                     setup_type_element,
+                     mcf_list,
+                     simple_element("CkpointNum", "xs:int",
+                                    element_dictionary),
+                     simple_element("ParameterInitialFile",
+                                    "nonEmptyString",
+                                    element_dictionary),
+                   ]
+
+
+  setup_element = ContainerElement("Setup", setup_children)
+  element_dictionary[setup_element.name] = setup_element
+  return setup_element
+
+#        <xs:element ref="sbsi_ofwconfig:MaxCost" />
+#        <xs:element ref="sbsi_ofwconfig:MaxRandomNumberGenerator" />
+#        <xs:element ref="sbsi_ofwconfig:SetupType" />
+#        <xs:element ref="sbsi_ofwconfig:CkpointNum" minOccurs="0" />
+#        <xs:element ref="sbsi_ofwconfig:ModelCostFunction" maxOccurs="unbounded"/>
+#        <xs:element ref="sbsi_ofwconfig:ParameterInitialFile" />
+ 
+
 def get_element_dictionary():
   """we build up a list of elements for which class definitions
      must be output. this avoids us outputting two class definitions
@@ -227,23 +407,15 @@ def get_element_dictionary():
      there should be"""
   # we keep a mapping from names to 
   element_dictionary = dict()
-  # This function appears to be being written from the bottom up
-  # I didn't really mean this to be the case, but ah well dem's
-  # the breaks.
-  t_final_simple_type = "positiveDecimalNotZeroButCanBeMinusOne"
-  t_final_element = LiteralElement("TFinal", t_final_simple_type) 
-  element_dictionary[t_final_element.name] = t_final_element
-
-  cvode_children = [t_final_element]
-  cvode_element = ContainerElement("CVODESolver", cvode_children)
-  element_dictionary[cvode_element.name] = cvode_element
+  cvode_element = get_cvode_solver_element(element_dictionary)
 
   solver_choices = [ cvode_element ]
   solver_element = OneOfElement("Solver", solver_choices)
   element_dictionary[solver_element.name] = solver_element
 
+  setup_element = get_setup_element(element_dictionary)
 
-  config_children = [ solver_element ]
+  config_children = [ solver_element, setup_element ]
   config_element = ContainerElement("OFWConfig", config_children)
   element_dictionary[config_element.name] = config_element
   return element_dictionary
@@ -272,6 +444,7 @@ def run():
   module_file = open ("configuration.py", "w")
 
   print_imports(module_file)
+  print_exception_definition(module_file)
 
   print_numerical_types(schema_file)
 
