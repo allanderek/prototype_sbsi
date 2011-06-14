2cdbc6b65e4bafcd89eea61b32e9aef9b8a83183
Allan Clark <a.d.clark@ed.ac.uk>
2011-06-14 15:29:49 +0100

    Further fiddling with fft cost function stuff

--- optimiser.py
+++ optimiser.py
@@ -28,7 +28,8 @@ def get_single_option(option_name, arguments):
   if not options:
     return ""
   elif len(options) > 1:
-    print ("Conflicting " + option_name + " arguments, choose one, exiting")
+    print ("Conflicting " + option_name +
+           " arguments, choose one, exiting")
     sys.exit(1)
   else:
     return options[0]
@@ -163,37 +164,63 @@ class FFTcost:
     # may be periodic
     self.state_names = gold_ts.get_column_names()
 
+
   @staticmethod
-  def dft(x, k):
-    N = len(x)
+  def dft_single(x, k):
     inv = -1 
+    N = len(x)
     Xk = 0
     for n in xrange(N) :
       Xk += x[n] * math.e**(inv * 2j * math.pi * k * n / N)
     Xk = Xk / N
-    return Xk
+    return abs(Xk)
+ 
+  @staticmethod
+  def dft(x, kman):
+    N = len(x)
 
+    # Rather than find the energy at all frequencies, just
+    # figure out the energy at a few selected points 
+    k1_energy = FFTcost.dft_single(x, 1)
+    kman_energy = FFTcost.dft_single(x, kman)
+    khalf_energy = FFTcost.dft_single(x, N / 2)
+    kquarter_energy = FFTcost.dft_single(x, N / 4)
+    energies = [ k1_energy, kman_energy, 
+                 khalf_energy, kquarter_energy ]
+    # energies = [ FFTcost.dft_single(x, n) for n in range(1, N / 2) ]
+    # The cost then is how much of the energy at all the frequencies
+    # is attributable to the energy at the frequency we are interested
+    # in. This is essentially saying how relatively well represented is
+    # the given frequency. Note there may be a corner case here in which
+    # N is less than the number of frequencies we are considering but
+    # I think that is not interesting for our purposes.
+    sum_energies = sum(energies)
+    # Must be positive since all energies are positive and the sum of
+    # all the energies must be at least as large as the single energy
+    # we are considering.
+    cost = (sum_energies / kman_energy) - kman_energy
+    logging.debug("dft_energy       = " + str(kman_energy))
+    logging.debug("total_dft_energy = " + str(sum_energies))
+    return cost
 
   def compare_timeseries(self, candidate_ts):
     """Evaluate the given time series based on this DFT cost"""
     # So the target period is actually 
-    start_sampling = 30.0
-    stop_sampling  = 200.0
+    # Should automatically check if these are in the output region
+    # of the solver since otherwise we will miss up.
+    start_sampling = 100.0
+    stop_sampling  = 300.0
     target_period  = 20.0
     num_cycles     = (stop_sampling - start_sampling) / target_period
     # 300 / 20 = 15 
     # num_cycles = 10 # 15 
     cost = 0
-    for column_name in self.state_names:
+    for column_name in ["mRNA"]: # self.state_names:
       column_data = candidate_ts.get_column_data(column_name)
       
-      dft_energy = self.dft(column_data, num_cycles)
-      logging.info("dft_energy = " + str(dft_energy))
-      absolute_energy = abs(dft_energy)
-      logging.info("dft_absolute = " + str(absolute_energy))
-      this_cost = 100 - absolute_energy
-      cost += this_cost 
-      logging.info("dft_cost(" + column_name + ") = " + str(this_cost))
+      dft_cost = self.dft(column_data, num_cycles)
+      logging.debug("dft_cost = " + str(dft_cost))
+      cost += dft_cost 
     return cost
 
 # Returns the indexes into candidate time series which correspond
@@ -657,7 +684,6 @@ class Optimisation:
     self.model_file = model_file
     self.gold_standard = gold_standard
     self.parameters = parameters
-
   def initialise(self):
     """Initialise the optimisation process"""
     pass
@@ -723,7 +749,7 @@ class SimulatedAnnealing:
   @staticmethod
   def mutate_parameter(change_param, current_value, temperature):
     """Given a parameter to change, decide on its new value."""
-        # The temperature is evals/maxevals, so how far through
+    # The temperature is evals/maxevals, so how far through
     # the maximum search range we are. Hence we want 1 minus that
     # to give us the proportion of the range that we should choose
     # from. 
