c4b7bd8929fba13a9e9f459f34d328d913f36f10
Allan Clark <a.d.clark@ed.ac.uk>
2011-06-30 14:14:32 +0100

    Added failing test for a 0.0 low range with something very close to zero

--- parameters_test.py
+++ parameters_test.py
@@ -19,18 +19,20 @@ class TestCheckParameters(unittest.TestCase):
   def setUp(self):
     # Similarly we can't do much about the name 'setUp' 
     # pylint: disable-msg=C0103
-    self.param_p1 = parameters.Parameter("p1", 1.0,
-                                         0.1, 10.0)
-    self.best_params = dict()
-    self.best_params["p1"] = 3.0
     self.arguments = Arguments()
 
   def test_check_param(self):
     """Checks the parameters.check_parameter function"""
-    p1_result = parameters.check_parameter(self.param_p1,
-                                           self.best_params["p1"], 
+    param_p1  = parameters.Parameter("p1", 1.0, 0.1, 10.0)
+    p1_result = parameters.check_parameter(param_p1, 3.0,
                                            self.arguments) 
     self.assertIsNone(p1_result)
+
+    param_p2  = parameters.Parameter("p2", 1.0, 0.0, 10.0)
+    p2_result = parameters.check_parameter(param_p2, 0.00001,
+                                           self.arguments)
+    self.assertIsNotNone(p2_result)
+    self.assertIsTrue(p2_result.too_low)
 
 if __name__ == '__main__':
   unittest.main()
