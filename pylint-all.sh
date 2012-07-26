58bd2bc58d5ab2fc33c58676852209fe1a64c12a
Allan Clark <a.d.clark@ed.ac.uk>
2012-07-26 09:46:10 +0100

    Added the pylint-all script

---
+++ pylint-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PYFILES="*.py biopepa/*.py facile/*.py BioModels_Database-r22-sbml_files/*.py"
+
+pylint -r no ${PYFILES}
+while getopts ":p" opt; do
+  case $opt in
+    p)
+      pychecker ${PYFILES}     
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
